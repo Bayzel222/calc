6bafcc59362b13ea0995756cb19e5abbae18e75b
Vasiliy Evlampev <Bayzel222@mail.ru>
2021-03-12 21:12:04 +0300

    домашка

---
+++ hello world.py
@@ -0,0 +1,6 @@
+Python 3.10.0a5 (v3.10.0a5:b0478d738a, Feb  2 2021, 16:30:44) 
+[Clang 12.0.0 (clang-1200.0.32.29)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("hello world")
+hello world
+>>> 
